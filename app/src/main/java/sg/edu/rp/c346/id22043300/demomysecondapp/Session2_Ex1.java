93f927c11c7a7ebde22f979ca1cdb3843b0b04f2
22043300 <22043300@myrp.edu.sg>
2023-04-25 14:51:19 +0800

    Included Activity Session 2 Exercise 1

---
+++ app/src/main/java/sg/edu/rp/c346/id22043300/demomysecondapp/Session2_Ex1.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id22043300.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Session2_Ex1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session2_ex1);
+    }
+}
